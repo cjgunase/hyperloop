0bd7ce42be28c8bbc560fb138a26e8ebfe4b15a0
cjgunase <cjgunase@mtu.edu>
2016-11-01 17:54:46 -0400

    ...

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hyperloop'
+    version='0.1',
+    description='All the hype now',
+    author='Chathura',
+    author_email='cjgunase@mtu.edu',
+    url='cjgunase.github.io',
+    packages=['hyperloop'],
+    install_requires=[
+    'hypoloop',
+    'geek'
+    ]
+)
